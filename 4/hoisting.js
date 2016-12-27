d21a481b270fb7f20dcac2c6a9f74744ca0a0174
Stina Qvarnström <stinaqv@gmail.com>
2016-12-27 10:12:24 +0100

    Assets for hoisting

---
+++ 4/hoisting.js
@@ -0,0 +1,92 @@
+(function() {
+  'use strict';
+
+  console.log(myNewVariable);
+  var myNewVariable = 'hej';
+
+}());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(function() {
+  'use strict';
+  /* 
+  Med hjälp av hoisting så är det föregående exemplet exakt
+  samma som att skriva så här
+  */
+  var myNewVariable;
+  console.log(myNewVariable);
+  myNewVariable = 'hej';
+
+}());
+
+(function() {
+  'use strict';
+  /* 
+  Därför är det best practice att deklarera alla sina variabler
+  i toppen av scope
+  */
+  var myNewVariable;
+  var myName = 'Stina';
+  var allAnimals = ['Horse', 'Unicorn', 'Pony'];
+  var favouriteShow;
+
+  function declareFavouriteShow() {
+    // För att sedan tilldela dem värde
+    favouriteShow = 'West Wing';
+  }
+
+}());
+
+(function() {
+  'use strict';
+  /* 
+  Även funktioner är hoistade
+  */
+
+  var favouriteShow;
+
+  // Så funktionen går att köra här, innan den är deklarerad
+  declareFavouriteShow();
+
+  function declareFavouriteShow() {
+    favouriteShow = 'West Wing';
+    console.log('My favourite show: ' + favouriteShow);
+  }
+
+}());
+
+
