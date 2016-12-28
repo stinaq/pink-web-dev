67778daad17d8044031b16082e86fa4ccc5ae248
Stina Qvarnström <stinaqv@gmail.com>
2016-12-28 13:41:37 +0100

    Assets for callbacks

---
+++ 4/callbacks.js
@@ -0,0 +1,18 @@
+(function() {
+  'use strict';
+
+  // function showSurprise() {
+  //   alert('Surprise');
+
+  //   return function() {
+  //     console.log('stina');
+  //   };
+  // }
+
+  // document
+  //   .querySelector('#surprise-button')
+  //   .addEventListener('click', showSurprise());
+
+  // setTimeout(showSurprise, 5000);
+
+}());
