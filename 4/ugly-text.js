3175b092feb498c3258029f0982e7792e59ce299
Stina Qvarnström <stinaqv@gmail.com>
2016-12-30 09:39:05 +0100

    Assets for uglify text task

---
+++ 4/ugly-text.js
@@ -0,0 +1,34 @@
+(function() {
+  'use strict';
+
+  function leftPadWithText(text) {
+
+    return '';
+  }
+
+  function replaceKattWithHund(text) {
+    var replacedText = text.replace('katt', 'hund');
+    return replacedText;
+  }
+
+  function addStinaEnding(text) {
+    var textWithEnding;
+
+    if (text.includes('Stina')) {
+      textWithEnding = text + ' Text om Stina';
+    }
+    else {
+      textWithEnding = text;
+    }
+
+    return textWithEnding;
+  }
+
+  function uglyfyText(originalText) {
+    var modifiedText = '';
+
+
+    return modifiedText;
+  }
+
+}());
