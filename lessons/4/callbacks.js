68815087c64218b100ffd65022cbb152f5427557
Stina Qvarnström <stinaqv@gmail.com>
2017-01-14 18:19:36 +0100

    Assets for callbacks

--- lessons/4/callbacks.js
+++ lessons/4/callbacks.js
@@ -1,18 +1,20 @@
 (function() {
   'use strict';
+  /*
+  Hur man kan använda en namngiven funktion som callback
+  till två olika saker
+  Funktionen showSurprise körs här både av setTimeout efter
+  5 sekunder, och när man klickar på knappen
+  */
 
-  // function showSurprise() {
-  //   alert('Surprise');
+  function showSurprise() {
+    alert('Surprise!');
+  }
 
-  //   return function() {
-  //     console.log('stina');
-  //   };
-  // }
+  document
+    .querySelector('#surprise-button')
+    .addEventListener('click', showSurprise);
 
-  // document
-  //   .querySelector('#surprise-button')
-  //   .addEventListener('click', showSurprise());
-
-  // setTimeout(showSurprise, 5000);
+  setTimeout(showSurprise, 5000);
 
 }());
