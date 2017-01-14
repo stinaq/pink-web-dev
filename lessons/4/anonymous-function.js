7c4a04b6788110ff549329c9cacf00da7c764bd1
Stina Qvarnström <stinaqv@gmail.com>
2017-01-14 18:22:08 +0100

    Anonymous function

---
+++ lessons/4/anonymous-function.js
@@ -0,0 +1,14 @@
+(function() {
+  'use strict';
+  /*
+  Anonym funktion
+  */
+
+  var fruits = ['apple', 'banana'];
+
+  fruits.forEach(function(item) {
+    console.log(item);
+  });
+
+  
+}());
