e1252cb63a3d0ed01114fb31546fccaa15b1d342
Stina Qvarnström <stinaqv@gmail.com>
2017-01-14 10:57:53 +0100

    Assets for DOM-manipulation

--- lessons/3/dom-manipulation.js
+++ lessons/3/dom-manipulation.js
@@ -1,17 +1,79 @@
 (function(){
   'use strict';
 
-
-  var input = document.querySelector('#validatable-input');
-  console.log(input);
-  input.addEventListener('blur', function() {
-    
+  // A click handler that logs a text to the console
+  var button = document.querySelector('#button-to-click');
+  button.addEventListener('click', function() {
+    console.log('Du klickade!');
   });
 
-  $('#validatable-input').on('keyup', function(test) {
-    console.log($('#validatable-input').val());
-
-    var thisInput = $('#validatable-input');
-    $('#welcome-text').text(thisInput.val());
-  })
+  // A handler that reacts on blur
+  var inputField = document.querySelector('#validatable-input');
+  inputField.addEventListener('blur', function() {
+    var text = inputField.value;
+    console.log(text);
+        
+    if (text.length < 10) {
+      var errorMessage = document.createElement('p');
+      errorMessage.textContent = 'För kort ord';
+      var parent = inputField.parentNode;
+      parent.appendChild(errorMessage);
+    }
+  });
+  
 })();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $('#validatable-input').on('keyup', function(test) {
+//     console.log($('#validatable-input').val());
+
+//     var thisInput = $('#validatable-input');
+//     $('#welcome-text').text(thisInput.val());
+//   })
+
+
