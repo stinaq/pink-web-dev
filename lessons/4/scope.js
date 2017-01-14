2518702cb9544a4edfb38caa971160f64503c581
Stina Qvarnström <stinaqv@gmail.com>
2017-01-14 10:57:20 +0100

    Assets for scoping

--- lessons/4/scope.js
+++ lessons/4/scope.js
@@ -1,32 +1,40 @@
-function defineMyName() {
-  'use strict';
-  var myName = "Stina";
-  horse = 'fe';
+
+/*
+An example of how scopes work
+What will it log at the end? 
+*/
+var name = 'Kajsa';
+var surname = 'Svensson';
+
+console.log(name);
+console.log(surname);
+
+function printName() {
+  var name = 'Sandra';
+  surname = 'Persson';
+  console.log(name);
 }
 
+printName();
+
+console.log(name);
+console.log(surname);
 
 
-
-var word = "Hello";
+/*
+An example of nested scopes. 
+*/
+var word = 'Hello';
 
 function speak() {
-  var word = "Hej";
-  console.log("In speak again");
+  var word = 'Hej';
+  console.log('In speak again');
   console.log(word);
 
   function speakAgain () {
-    console.log("In speak again");
+    console.log('In speak again');
     console.log(word);
   }
 }
 
 speak();
-
-function printHorse() {
-  'use strict';
-  
-  horseName = 'Goldie';
-  console.log(horseName);
-}
-
-printHorse();
