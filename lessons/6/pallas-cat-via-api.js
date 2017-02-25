fabfc181c14fd2f74262a745705779fb577804a5
Stina Qvarnström <stinaqv@gmail.com>
2017-02-25 13:11:11 +0100

    Updating pallas cat example with PUT-request

--- lessons/6/pallas-cat-via-api.js
+++ lessons/6/pallas-cat-via-api.js
@@ -2,26 +2,66 @@
   'use strict';
 
   var url = 'https://api.myjson.com/bins/x66lp';
+  var animal;
 
-  function displayCharacters(data) {
-    var characterList = document.querySelector('#character-list');
+  fetchData();
+  setUpChangeUrl();
 
-    // This is to empty the list, so that we don't just add duplicates
-    characterList.innerHTML = '';
-
-    data.forEach(function(item) {
-      var charachterElement = document.createElement('li');
-      charachterElement.textContent = item.name;
-      characterList.appendChild(charachterElement);
-    });
+  function fetchData() {
+    fetch(url)
+      .then(function(response){
+        return response.json();
+      })
+      .then(function(data) {
+        animal = data;
+        setUpData();
+      });
   }
 
-  fetch(url)
-    .then(function(response){
-      return response.json();
-    })
-    .then(function(data) {
-      displayCharacters(data);
+  function setUpData() {
+    var headlineElement = document.querySelector('#headline');
+    headline.textContent = animal.species;
+
+    var latinNameElement = document.querySelector('#latin-name');
+    latinNameElement.textContent = animal.latinName;
+
+    var imageElement = document.querySelector('#image');
+    imageElement.src = animal.imageUrl;
+
+    var characteristicsElement = document.querySelector('#characteristics');
+    characteristicsElement.textContent = animal.characteristics;
+  }
+
+  function updateImgUrlData(newImgUrl) {
+    var copyOfAnimal = Object.assign({}, animal);
+    copyOfAnimal.imageUrl = newImgUrl;
+
+    var headers = new Headers({
+      "Content-Type": "application/json; charset=utf-8"
     });
+    
+    var fetchData = { 
+      method: 'PUT', 
+      body: JSON.stringify(copyOfAnimal),
+      headers: headers
+    };
+
+    fetch(url, fetchData)
+      .then(function(response){
+        return response.json();
+      })
+      .then(function(data) {
+        animal = data;
+        setUpData();
+      });
+  }
+
+  function setUpChangeUrl() {
+    var newImgButtonElement = document.querySelector('#new-image-url-button');
+    newImgButtonElement.addEventListener('click', function() {
+      var newImgUrl = document.querySelector('#new-image-url').value;
+      updateImgUrlData(newImgUrl);
+    });  
+  }
   
 })();
