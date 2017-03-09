7d17ecbd2abe70e5db986b79dcca08d7af9eefed
Stina Qvarnström <stinaqv@gmail.com>
2017-03-09 17:39:31 +0100

    Assets for debugging

---
+++ lessons/7/debugger.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict';
+
+  var age = 25;
+
+  if (age < 25) {
+    console.log('du är äldre än 25');
+  } else {
+    console.log('du är yngre än 25');
+  }
+  
+  console.log('Det här är slutet av filen om errors');
+})();
