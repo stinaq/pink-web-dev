ee48a652091df9c25efef7c2d6d17540631df663
Stina Qvarnström <stinaqv@gmail.com>
2017-03-10 21:32:01 +0100

    Working version of importing data to all user databases

--- excercises/harry-potter-db/create-basedata.js
+++ excercises/harry-potter-db/create-basedata.js
@@ -5,61 +5,72 @@
   const baseUrl = apiUrl;
   const baseData = hpBaseData;
 
-  let promisesOfAllUsers = userNames.map(userName => {
-    return fetch(`${baseUrl}?key=${userName}`)
-    .then(response => response.json())
-    .then(data => {
-      return {
-        userName: userName,
-        characters: data        
-      };
-    })
-  });
+  function deleteAllCurrentData() {
+    let promisesOfAllUsers = userNames.map(userName => {
+      return fetch(`${baseUrl}?key=${userName}`)
+      .then(response => response.json())
+      .then(data => {
+        return {
+          userName: userName,
+          characters: data        
+        };
+      });
+    });
 
-  Promise.all(promisesOfAllUsers)
-  .then((allGets) => {
+    Promise.all(promisesOfAllUsers)
+    .then((allGets) => {
 
-    allGets.map((gottenUser) => {
-      console.log(gottenUser.userName);
-      const userKey = gottenUser.userName;
-      gottenUser.characters.forEach((character) => {
+      allGets.map((gottenUser) => {
+        console.log(`This is user ${gottenUser.userName}`);
+        const userKey = gottenUser.userName;
+        gottenUser.characters.forEach((character) => {
+          console.log('Found a character and are removing it');
+          var headers = new Headers({
+            "Content-Type": "application/json; charset=utf-8"
+          });
+
+          var fetchData = {
+            method: 'DELETE',
+            headers: headers
+          };
+
+          fetch(`${baseUrl}/${character._id}?key=${userKey}`, fetchData)
+          .then(response => {
+            console.log(response);
+          });
+        });
+      });
+    });
+  }
+
+  function createAllCharacters() {
+
+    let promisesOfCharacters = userNames.map((userName) => {
+      return baseData.map((character) => {
         var headers = new Headers({
           "Content-Type": "application/json; charset=utf-8"
         });
 
         var fetchData = {
-          method: 'DELETE',
+          method: 'POST',
+          body: JSON.stringify(character),
           headers: headers
         };
 
-        fetch(`${baseUrl}/${character._id}?key=${userKey}`, fetchData)
+        return fetch(`${baseUrl}?key=${userName}`, fetchData)
         .then(response => {
           console.log(response);
         });
       });
     });
-  });
 
-
-  function createCharacter(userKey, character) {
-    var headers = new Headers({
-      "Content-Type": "application/json; charset=utf-8"
-    });
-
-    // Sätt ihop alla delarna
-    var fetchData = {
-      method: 'POST',
-      body: JSON.stringify(character),
-      headers: headers
-    };
-
-    fetch(`${baseUrl}?key=${userKey}`, fetchData)
-    .then(response => {
-      console.log(response);
-    });
+    Promise.all(promisesOfCharacters)
+    .then(() => {
+      console.log('all posts are done');
+    })
   }
 
-  // function createAllCharacters
+  // deleteAllCurrentData();
+  createAllCharacters();
 
-  // createCharacter('stinaq', baseData[0])
 })();
