cc52d65c87c3d56545850f88e9522bd9e3cec7db
Stina Qvarnström <stinaqv@gmail.com>
2017-04-10 21:09:10 +0200

    Webapp seed project

--- excercises/pinkwebdev-seed/src/main.js
+++ excercises/pinkwebdev-seed/src/main.js
@@ -2,5 +2,5 @@
   var element = document.createElement('h2');
   element.textContent = 'and hello JS';
   document.body.appendChild(element);
-  console.log('her');
+  console.log('DET HÄR ÄR FIL 1');
 })();
---
+++ excercises/pinkwebdev-seed/src/main2.js
@@ -0,0 +1,1 @@
+console.log('DET HÄR ÄR FIL 2');
